bce8495549854fe669b6abd19528d1a93737222a
naxer-12 <rexan.2212@gmail.com>
2022-10-14 11:33:42 -0400

    initial commit for wallet App

---
+++ screens/Welcome.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from "react";
+import { StatusBar } from "expo-status-bar";
+import styled from "styled-components/native";
+
+const Welcome:FunctionComponent = () => {
+  return <>
+  
+  <StatusBar style="light"/>
+  
+  </>;
+};
+export default Welcome;
